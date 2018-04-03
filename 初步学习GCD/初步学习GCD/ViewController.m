a3f167d34155bdf76b8bfe00a806bf379327b367
fengshuo_1 <963787902@qq.com>
2018-04-03 18:57:03 +0800

    李四在修改bug的分支修改了bug

--- 初步学习GCD/初步学习GCD/ViewController.m
+++ 初步学习GCD/初步学习GCD/ViewController.m
@@ -43,6 +43,9 @@
     NSLog(@"fixbugHot分支");
     
     
+    NSLog(@"李四修改了东西");
+    
+    
 //    STSegmentView *exampleSegmentView = [[STSegmentView alloc]initWithFrame:CGRectMake(0, 30, self.view.bounds.size.width, 50)];
 //    exampleSegmentView.titleArray = @[@"test1",@"test2",@"test3",@"test4", @"test5", @"test6",@"test7",@"test8",@"test9"];
 //    exampleSegmentView.selectedBackgroundColor = [UIColor redColor];
