a5250aef98602e15dc5556320cff921562b1fd13
fengshuo_1 <963787902@qq.com>
2018-04-03 19:23:10 +0800

    FixTestView

---
+++ 初步学习GCD/初步学习GCD/FixTestView.m
@@ -0,0 +1,31 @@
+//
+//  FixTestView.m
+//  初步学习GCD
+//
+//  Created by gzlx on 2018/4/3.
+//  Copyright © 2018年 冯硕. All rights reserved.
+//
+
+#import "FixTestView.h"
+
+@implementation FixTestView
+
+
+
+- (id)initWithFrame:(CGRect)frame
+{
+    if (self = [super initWithFrame:frame]) {
+        
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
